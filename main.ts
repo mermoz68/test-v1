ab30173e3331fb8fccba0103804761f20c547ef9
mermoz68 <47685555+mermoz68@users.noreply.github.com>
2023-05-05 10:33:16 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.showLeds(`
+    . . . . #
+    . . . # .
+    . . . . .
+    . # . . .
+    # . . . .
+    `)
+basic.pause(1000)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(200)
+})
