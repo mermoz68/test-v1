a10091d55c039f817754b4f65b0fd8ff2724d2aa
mermoz68 <47685555+mermoz68@users.noreply.github.com>
2023-05-05 10:36:34 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 basic.showLeds(`
-    . . . . #
-    . . . # .
     . . . . .
-    . # . . .
-    # . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
     `)
 basic.pause(1000)
 basic.forever(function () {
